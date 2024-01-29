a21c822ce15c0ed84827cf53b90f40fc017467b6
rweil55 <github@shaw-weil.com>
2024-01-28 20:54:58 -0500

    from indesign 2024-01-29 01:54

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,7 @@
 
 cd /inetpub/wwwroot/w/tredway
 git add .
-git commit -m "from indesign 2024-01-28 23:34" --no-edit
+git commit -m "from indesign 2024-01-29 01:54" --no-edit
 git push https://github.com/rweil55/web-tredway
 
 rem wait for one minute to allow git hub to get its act together
